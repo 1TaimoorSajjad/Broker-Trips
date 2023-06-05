c367e5730a337972ecacfd07f458c131a88c91d7
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-05 12:15:56 +0500

    Collection Added

--- src/app/Components/brokerdisp/brokerdisp.component.ts
+++ src/app/Components/brokerdisp/brokerdisp.component.ts
@@ -2,13 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+interface BrokerFormData {
+  id: string;
+  broker: string;
+  clientName: string;
+  tripId: string;
+  insuranceId: string;
+  clientId: string;
+  phoneNumber: string;
+  cellNumber: string;
+  pickAddress: string;
+  dropAddress: string;
+  state: string;
+  miles: string;
+  cNotes: string;
+  escorts: string;
+  legs: Leg[];
+}
+
+interface Leg {
+  pickAddress: string;
+  dropAddress: string;
+}
+
 @Component({
   selector: 'app-brokerdisp',
   templateUrl: './brokerdisp.component.html',
   styleUrls: ['./brokerdisp.component.css']
 })
 export class BrokerdispComponent implements OnInit {
-  dispData: any;
+  dispData: BrokerFormData[] = [];
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -27,7 +50,7 @@ export class BrokerdispComponent implements OnInit {
     });
   }
 
-  editData( id: string) {
+  editData(id: string) {
     this.router.navigate(['/brokerform/edit', id]);
   }
 }
